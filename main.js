4865472e1e9798ddd105f0f8aae773e39da51c47
Mimi <mimichs@outlook.com>
2020-07-22 13:21:58 -0700

    7/22/20

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ function init(){
  })
 
  const pinStyle = new ol.style.Icon({
-    scale: 0.7,
-    src: '//raw.githubusercontent.com/jonataswalker/map-utils/master/images/marker_3cc483.png'
+    scale: 0.6,
+    src: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png'
  })
 
  const SitesGeoJSON = new ol.layer.VectorImage({
